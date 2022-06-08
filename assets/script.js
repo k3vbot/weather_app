c802bc0ea1b9c5ee0b9aca33a0213c9ec2b1657b
Kevin <muehl004@gmail.com>
2022-06-07 18:36:47 -0700

    prevent default on search

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
+$(".search").click(function(e){
+    e.preventDefault();
+    const inputVal = input.value 
+})
+
 const APIKey = "4523ea8a862fd071298a6b39372ecf26";
 let city = "";
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=' + city + &appid=" + APIKey;
+const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
 
 fetch(queryURL)
