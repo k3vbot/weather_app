6ceac9d2a7f7d8b1bfd7ed9dac8d9450b87f61df
Kevin <muehl004@gmail.com>
2022-06-08 21:51:06 -0700

    started fetch request

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,18 @@ $(".search").click(function(e){
     e.preventDefault();
 })
 
-const APIKey = "4523ea8a862fd071298a6b39372ecf26";
+const weatherAPIKey = "4523ea8a862fd071298a6b39372ecf26";
 let city = "";
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-const inputVal = input.value
+inputVal = input.value
+const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}&units=imperial';
 
 fetch(queryURL)
+    .then(response => response.json())
+    .then(data => {
+
+    })
+    .catch(() => {
+        msg.textContent = "Please search for a valid city";
+    });
+
 
