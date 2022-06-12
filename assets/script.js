8bc558160868414838da7f9c2176fd5dfb9c5004
Kevin <muehl004@gmail.com>
2022-06-12 11:39:16 -0500

    minor html and js changes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 $(".search").click(function(e){
     e.preventDefault();
+    const inputVal = input.value
 })
 
 const weatherAPIKey = "4523ea8a862fd071298a6b39372ecf26";
 let city = "";
-inputVal = input.value
+
 const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}&units=imperial';
 
 fetch(queryURL)
