b3ca21aa8a79acd88f68d22f3dcb7dc0bb704195
Kevin <muehl004@gmail.com>
2022-06-12 22:07:35 -0500

    local storage function

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
+const weatherAPIKey = "4523ea8a862fd071298a6b39372ecf26";
+let cityList = "";
+
+function cityStorage() {
+    localStorage.setItem("cities", cityList);
+}
+
+function buildCityList() {
+    $(".cityList").empty();
+    cityList.forEach(function(city){
+        $(".cityList").prepend($('<button class = "list-group-item list-group-item-action cityBtn" data-city="${city}">${city}</button>'));
+    })
+}
+const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}&units=imperial';
+
+
+
 $(".submit").click(function(e){
     e.preventDefault();
     const inputVal = input.value
 })
-
-const weatherAPIKey = "4523ea8a862fd071298a6b39372ecf26";
-let city = "";
-
-const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}&units=imperial';
-
-fetch(queryURL)
-    .then(response => response.json())
-    .then(data => {
-
-    })
-    .catch(() => {
-        msg.textContent = "Please search for a valid city";
-    });
-
-
