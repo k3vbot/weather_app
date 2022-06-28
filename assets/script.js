aea853afa1e9ee32e3cd66f95ed94b23688b85d5
Kevin <muehl004@gmail.com>
2022-06-27 22:25:09 -0700

    fixed issue with json.stringify

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 //create variable to store API key
-const weatherAPIKey = "3ee538646a50b1b127aaf88e7856028d";
-//create empty string for cities
-let cityList = [''];
+const weatherAPIKey = "225851f9311f6df037b641b3c403576b";
+//create empty array for cities
+let cityList = [ ];
 
 //set items in local storage
 function cityStorage() {
-    localStorage.setItem("cities", cityList);
+    localStorage.setItem("cities", JSON.stringify(cityList));
 }
 
 //create buttons for searched cities
-function buildCityList() {
+function buildCityList(){
     $(".cityList").empty();
     cityList.forEach(function(city){
-        $(".cityList").prepend($('<button class = "list-group-item list-group-item-action cityBtn" data-city="${city}">${city}</button>'));
+        $(".cityList").prepend($(`<button class = "list-group-item list-group-item-action cityBtn" data-city="${city}">${city}</button>`));
     })
 }
 //initialize function to get searched cities from local storage
@@ -34,7 +34,7 @@ function init() {
 
 //build url to get the info from weather API and display info
 function getTheWeather(thisCity, weatherAPIKey) {
-    const queryURL = 'https://api.openweathermap.org/data/3.0/onecall?${inputVal}&appid=${weatherAPIKey}&units=imperial';
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=imperial&appid=${weatherAPIKey}`;
     let cityLat;
     let cityLong;
 
@@ -52,14 +52,14 @@ function getTheWeather(thisCity, weatherAPIKey) {
         $(".weatherToday").append(`<p> Humidity: ${data.main.humidity}%</p>`)
         $(".weatherToday").append(`<p> Wind: ${data.wind.speed}mph</p>`)
         cityLat = data.coord.lat;
-        cityLong = data.coord.long;
+        cityLong = data.coord.lon;
         getUVIndex(weatherAPIKey, cityLat, cityLong);
     })
 }
 
 //5 day forecast
 function getForecast(thisCity, weatherAPIKey) {
-    let forecastURL = `https://api.openweathermap.org/data/3.0/forcast?q=${thisCity}&units=imperial&appid=${weatherAPIKey}`;
+    let forecastURL = `https://api.openweathermap.org/data/2.5/forcast?q=${thisCity}&units=imperial&appid=${weatherAPIKey}`;
 
     $.ajax({
         url: forecastURL,
@@ -87,7 +87,7 @@ function getForecast(thisCity, weatherAPIKey) {
 
 // function to get UV data
 function getUVIndex(weatherAPIKey, cityLat, cityLong) {
-    let uvUrl = `https://api.openweathermap.org/data/3.0/uvi?lat=${cityLat}&lon=${cityLong}&appid=${weatherAPIKey}`;
+    let uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityLat}&lon=${cityLong}&appid=${weatherAPIKey}`;
 
     $.ajax({
         url: uvUrl,
